8ea3453fe904ad28f9b1cb89112113d832f0521c
fl4viooliveira <fl4viooliveira@gmail.com>
2021-08-31 13:22:17 +0100

    React Router applyed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
-import Header from "./components/Header";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import PhoneList from "./pages/PhoneList";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <Home />
-      <PhoneList />
-      <Detail />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/list">
+          <PhoneList />
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
