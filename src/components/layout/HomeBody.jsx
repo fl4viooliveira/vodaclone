7ac5af752bb1c543265ab1319bdc5477e96c139a
fl4viooliveira <fl4viooliveira@gmail.com>
2021-09-06 22:39:33 +0100

    working on css

--- src/components/layout/HomeBody.jsx
+++ src/components/layout/HomeBody.jsx
@@ -3,10 +3,10 @@ import galaxy from "../../images/home/vfcon110904.jpg";
 import Guaranteed from "../data/Guaranteed";
 import YouMightLike from "../data/YouMightLike";
 import NowTrending from "../data/NowTrending";
-import LifeEasier from "../data/LifeEasier";
-import MoreToLove from "../data/MoreToLove";
-import VeryMe from "../../images/home/vfcon106076.png";
-import DiscoverMore from "../data/DiscoverMore";
+// import LifeEasier from "../data/LifeEasier";
+// import MoreToLove from "../data/MoreToLove";
+// import VeryMe from "../../images/home/vfcon106076.png";
+// import DiscoverMore from "../data/DiscoverMore";
 import Footer from "./Footer";
 
 export default function HomeBody(props) {
@@ -14,11 +14,15 @@ export default function HomeBody(props) {
     <div className="body-container">
       <img className="galaxy" src={galaxy} alt="Galaxy" />
       <Guaranteed />
-      <YouMightLike />
-      <NowTrending />
-      <LifeEasier />
-      <MoreToLove />
-      <div className="very-me">
+      <div className="yml-div">
+        <YouMightLike />
+      </div>
+      <div className="nt-div">
+        <NowTrending />
+      </div>
+      {/* <LifeEasier /> */}
+      {/* <MoreToLove /> */}
+      {/* <div className="very-me">
         <h1>
           VeryMe Rewards <br /> The loyalty programme built around you
         </h1>
@@ -26,8 +30,8 @@ export default function HomeBody(props) {
           <a href="#0">Find out more</a>
         </button>
         <img src={VeryMe} alt="VeryMe" />
-      </div>
-      <DiscoverMore />
+      </div> */}
+      {/* <DiscoverMore /> */}
       <Footer />
     </div>
   );
