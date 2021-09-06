5e9a1ccb59fec932a3c39cfc3b29a1151aabc43d
fl4viooliveira <fl4viooliveira@gmail.com>
2021-09-06 23:24:27 +0100

    working on css

--- src/components/layout/HomeBody.jsx
+++ src/components/layout/HomeBody.jsx
@@ -2,24 +2,20 @@ import "./HomeBody.css";
 import galaxy from "../../images/home/vfcon110904.jpg";
 import Guaranteed from "../data/Guaranteed";
 import YouMightLike from "../data/YouMightLike";
-import NowTrending from "../data/NowTrending";
+// import NowTrending from "../data/NowTrending";
 // import LifeEasier from "../data/LifeEasier";
 // import MoreToLove from "../data/MoreToLove";
 // import VeryMe from "../../images/home/vfcon106076.png";
 // import DiscoverMore from "../data/DiscoverMore";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 export default function HomeBody(props) {
   return (
     <div className="body-container">
       <img className="galaxy" src={galaxy} alt="Galaxy" />
       <Guaranteed />
-      <div className="yml-div">
-        <YouMightLike />
-      </div>
-      <div className="nt-div">
-        <NowTrending />
-      </div>
+      <YouMightLike />
+      {/* <NowTrending /> */}
       {/* <LifeEasier /> */}
       {/* <MoreToLove /> */}
       {/* <div className="very-me">
@@ -32,7 +28,7 @@ export default function HomeBody(props) {
         <img src={VeryMe} alt="VeryMe" />
       </div> */}
       {/* <DiscoverMore /> */}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
